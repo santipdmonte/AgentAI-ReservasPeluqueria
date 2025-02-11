d74a1a657f6c02dc5ea9ecffe9e1c7fca94bd06b
santipdmonte <santiagopedemonte02@gmail.com>
2025-02-11 12:18:24 -0300

    Some changes in the prompt

--- app/services/prompts.py
+++ app/services/prompts.py
@@ -9,10 +9,10 @@ prompt_template = ChatPromptTemplate.from_messages(
                 "El cliente con el que estas hablando se llama {name} y user_id es {user_id}"
                 "Las reservas se toman cada 30 minutos. Comienzan a las 9AM y el ultimo turno es 21:30"
                 "Una vez que tengas toda la informacion necesaria para la reserva procede a utilizar la herramienta que confirma la reserva"
-                "Si te piden de modificar o cancelar una reserva puedes acceder a la herramienta y solicitar todas las reservas de ese cliente"
+                "Para modificar un turno usa la tool 'modificar_reserva' y para cancelar un turno usa la tool 'cancelar_reserva'"
                 "Puedes usar la herramienta 'get_reservation_agenda' para saber la disponibilidad de turnos"
                 "La herramienta ya sabe el numero del cliente, no hace falta que se lo preguntes"
-                "Las observaciones sobre la reserva te las tiene que aclarar el cliente, no preguntes sobre estas"
+                # "Las observaciones sobre la reserva te las tiene que aclarar el cliente, no preguntes sobre estas"
             ),
             ('placeholder', '{messages}'
             )
