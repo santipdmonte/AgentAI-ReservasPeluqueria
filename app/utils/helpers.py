5275512c100804fcd5a2d2cda0b9416dcf66d030
santipdmonte <santiagopedemonte02@gmail.com>
2025-02-23 12:07:37 -0300

    Translate English day names to Spanish in date formatting; enhance current date and time retrieval for Argentina

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,13 +1,25 @@
 from datetime import datetime
 import pytz
-import locale
 
-locale.setlocale(locale.LC_TIME, "es_ES.utf8")  # Puede variar según el sistema operativo
+# Diccionario para traducir días de inglés a español
+dias_espanol = {
+    "Monday": "Lunes",
+    "Tuesday": "Martes",
+    "Wednesday": "Miércoles",
+    "Thursday": "Jueves",
+    "Friday": "Viernes",
+    "Saturday": "Sábado",
+    "Sunday": "Domingo"
+}
 
 # Definir la zona horaria de Argentina
 argentina_tz = pytz.timezone("America/Argentina/Buenos_Aires")
+
+# Obtener la fecha y hora actual en Argentina
 fecha_hora_actual = datetime.now(argentina_tz)
 
-# Obtener el nombre del día en español
-nombre_dia = fecha_hora_actual.strftime("%A").capitalize()
+# Obtener el nombre del día en inglés y traducirlo
+nombre_dia = dias_espanol[fecha_hora_actual.strftime("%A")]
+
+# Formatear la fecha y hora solo hasta minutos
 fecha_hora_actual = fecha_hora_actual.strftime("%Y-%m-%d %H:%M")
