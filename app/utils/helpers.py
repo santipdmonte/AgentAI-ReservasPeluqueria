73f20c8f4d449992f3d45491a1f816a6a5a59cdf
santipdmonte <santiagopedemonte02@gmail.com>
2025-02-23 11:37:32 -0300

    Set locale for date formatting in Spanish; update current date and time retrieval for Argentina

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 import pytz
+import locale
+
+locale.setlocale(locale.LC_TIME, "es_ES.utf8")  # Puede variar según el sistema operativo
 
 # Definir la zona horaria de Argentina
 argentina_tz = pytz.timezone("America/Argentina/Buenos_Aires")
-# Obtener la fecha y hora actual en Argentina
 fecha_hora_actual = datetime.now(argentina_tz)
+
 # Obtener el nombre del día en español
 nombre_dia = fecha_hora_actual.strftime("%A").capitalize()
+fecha_hora_actual = fecha_hora_actual.strftime("%Y-%m-%d %H:%M")
